0f19885d857e50fa7fbd03dacec611d8305b6b44
Adithyakrishnan <adithyakrishnannanda@gmail.com>
2022-07-23 22:04:27 +0530

    New app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeformComponent } from './recipeform/recipeform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeformComponent
   ],
   imports: [
     BrowserModule,
